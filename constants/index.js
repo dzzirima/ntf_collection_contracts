b455d196b7e97ec7cd344290e4c316138c6fa0d4
dzzirima <52695278+dzzirima@users.noreply.github.com>
2022-04-15 12:17:43 +0200

    feat :done with contracts

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xcbC08F510039B2f744ED5955cd235014A3254F9B";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
